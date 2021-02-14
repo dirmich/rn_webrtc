4227aa3ee230d2d729c8c77db434f969435d3818
David, Shin <dirmich@gmail.com>
2021-02-15 05:46:01 +0900

    init

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  ...require('prettier-airbnb-config'),
+  printWidth: 120,
+  bracketSpacing: false,
+  jsxBracketSameLine: true,
+  singleQuote: true,
+  trailingComma: 'es5',
+  semi: false,
+}
